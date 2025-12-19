982cef8d8fb5c65ba1be00b7b3e30a7ae4269247
SUNIL-RAJPUT-495 <sunilsinghshekhawat495@gmail.com>
2025-12-19 19:43:52 +0530

    core updated

--- server.js
+++ server.js
@@ -11,12 +11,23 @@ dotenv.config();
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://producter-dashboard-client-ymgd.vercel.app"
+];
+
 app.use(cors({
-    origin: process.env.FRONTEND_URL,   // http://localhost:5173
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true); // Postman etc.
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'CORS policy: This origin is not allowed';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
 }));
+
 app.use(express.json());
 app.use(morgan("dev"));
 
