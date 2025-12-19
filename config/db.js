65a9a95ce63b94ef5aefa036209e9059819fc734
Sunil Shekhawat <sunilshekhawat@gmail.com>
2025-12-20 00:22:04 +0530

    all ok

--- config/db.js
+++ config/db.js
@@ -1,12 +1,22 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 const connectDB = async () => {
+  if (isConnected) {
+    return;
+  }
+
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB Connected");
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "producter-dashboard"
+    });
+
+    isConnected = true;
+    console.log("MongoDB Connected");
   } catch (error) {
-    console.error("❌ DB Error:", error.message);
-    process.exit(1);
+    console.error("DB Error:", error.message);
+    throw error;
   }
 };
 
